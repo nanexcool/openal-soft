859d52c6c4b4c4d3ed37a86dda9b5a8071c0dbef
Steve Williams <dev@konaju.com>
2014-06-30 23:28:03 +1000

    Add new openal-soft code from Orx Game Engine.

---
+++ lib/android/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := OpenAL-prebuilt
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libopenal-soft.a
+LOCAL_EXPORT_LDLIBS = -lOpenSLES -llog
+TARGET_PLATFORM = android-9
+
+LOCAL_STATIC_LIBRARIES += cpufeatures
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+$(call import-module,android/cpufeatures)
